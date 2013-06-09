fbfcf64908e227ba98b7a4dbf895f9435dc2aa61
Alan Partridge <partridge.alan@outlook.com>
2013-06-09 21:55:01 +0100

    Changed pytail to act as a generator instead of using callbacks

--- pytail.py
+++ pytail.py
@@ -3,29 +3,26 @@ import os
 import time
 
 
-def default_cb(count, line):
-    print count, line
 
 class PyTail(object):
     """
     A polling version of the unix 'tail -F' command
     """
 
-    def __init__(self, filename, cb=None, pollint=0.5):
+    def __init__(self, filename, pollint=0.5, block=True):
         self.filename = filename
-        if cb:
-            self.cb = cb
-        else:
-            self.cb = default_cb
         self.pollint = pollint
+        self.block = block
         self.count = 0
         self.current_inode = None
 
     def read_to_end(self, f):
         for line in f.readlines():
-            self.cb(self.count, line)
+            yield line
             self.count += 1
-        return f.tell()
+
+        if not self.block:
+            yield None
 
     def has_changed(self):
         return os.stat(self.filename).st_ino != self.current_inode
@@ -40,17 +37,21 @@ class PyTail(object):
 
                 while True:
                     changed = self.has_changed()
-                    self.read_to_end(f)
+                    for line in self.read_to_end(f):
+                        yield line
                     if changed:
                         break
-                    time.sleep(0.5)
+                    time.sleep(self.pollint)
         except (IOError, OSError) as e:
             if e.errno != errno.ENOENT:
                 raise
 
     def tail(self):
         while True:
-            self.tail1()
-            time.sleep(0.5)
+            for line in self.tail1():
+                yield line
+            time.sleep(self.pollint)
 
+    def __iter__(self):
+        return self.tail()
 
