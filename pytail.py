99610746712c7ddda965e98abbcb8d71aba0254b
Alan Partridge <partridge.alan@outlook.com>
2013-06-15 23:26:31 +0100

    Keep the log start match object

--- pytail.py
+++ pytail.py
@@ -1,6 +1,7 @@
 import errno
 import os
 import time
+import logging
 
 
 
@@ -38,6 +39,7 @@ class PyTail(object):
                 while True:
                     changed = self.has_changed()
                     for line in self.read_to_end(f):
+                        logging.debug('Got line: %s', line)
                         yield line
                     if changed:
                         break
@@ -57,21 +59,23 @@ class PyTail(object):
 
 
 
-def default_message_cb(m):
-    print 'MESSAGE: %s' % m
+def default_message_cb(msg, match):
+    print 'MESSAGE: %s' % msg
 
 def default_log_start_f(line):
-    return not line.startswith(' ')
+    return (not line.startswith(' '), None)
 
 class LogParser(object):
 
     def __init__(self, filename, message_cb=default_message_cb,
-                 log_start_f=default_log_start_f):
-        self.tail = PyTail(filename, 2)
+                 log_start_f=default_log_start_f, pollint=2, block=True):
+        self.tail = PyTail(filename, pollint, block)
         self.message_cb = message_cb
         self.log_start_f = log_start_f
         self.current = None
         self.next = None
+        self.current_match = None
+        self.next_match = None
 
     def parse(self):
         self.current = self.next
@@ -79,20 +83,24 @@ class LogParser(object):
 
         for line in self.tail:
             if self.got_line(line):
-                self.message_cb(self.current)
+                self.message_cb(self.current, self.current_match)
                 self.current = self.next
+                self.current_match = self.next_match
                 self.next = None
 
     def got_line(self, line):
         if line is None and self.current is not None:
             return True
 
-        if self.log_start_f(line):
+        m, match = self.log_start_f(line)
+        if m:
             if self.current is None:
                 self.current = line
+                self.current_match = match
                 return False
             else:
                 self.next = line
+                self.next_match = match
                 return True
         else:
             if self.current is not None:
