ad84a448335fbf32228528067c2153feafd7eaf5
Alan Partridge <partridge.alan@outlook.com>
2013-06-15 23:27:04 +0100

    taillog wraps the pytail object

--- taillog.py
+++ taillog.py
@@ -1,18 +1,50 @@
 import pytail
 import re
+import logging
 
 
+class LogReporter(object):
 
-log_start_re = re.compile('^(?P<date>\d\d\d\d-\d\d-\d\d) '
-                          '(?P<time>\d\d:\d\d:\d\d,\d\d\d) '
-                          '(?P<level>\w+) ')
+    def __init__(self, file, name, arse, levels):
+        self.file = file
+        self.name = name
+        self.arse = arse
+        self.levels = levels
 
-def is_log_start(m):
-    return log_start_re.match(m) is not None
+        self.log_re = re.compile('^(?P<date>\d\d\d\d-\d\d-\d\d) '
+                                 '(?P<time>\d\d:\d\d:\d\d,\d\d\d) '
+                                 '(?P<level>\w+) ')
+        self.max_log_length = 4096
+        self.n_logs_notified = 0
 
-def taillog(filename):
-    #log = pytail.LogParser(filename, message_cb, log_start_f)
-    log = pytail.LogParser(filename, log_start_f=is_log_start)
-    log.parse()
+    def is_log_start(self, m):
+        logging.debug('is_log_start: %s', m)
+        match = self.log_re.match(m) if m else None
+        return (match is not None, match)
+
+    def log_received(self, msg, match):
+        logging.debug('log_received: %s', msg)
+        level = match.groupdict()['level']
+        if level in self.levels:
+            self.n_logs_notified += 1
+            m = '%s: %s:\n%s' % (level, self.name, msg[:self.max_log_length])
+            self.arse.log_message(m)
+
+    def taillog(self):
+        pollint = 2
+        block = False
+        log = pytail.LogParser(self.file, self.log_received, self.is_log_start,
+                               pollint, block)
+        log.parse()
+
+    def start(self):
+        self.taillog()
+
+    def status(self):
+        m = '%s: filename:%s\n\t%d notified' % (
+            self.name, self.file, self.n_logs_notified)
+        logging.debug('status: %s', m)
+        return m
+
 
 
